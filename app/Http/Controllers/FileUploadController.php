5ab991eb67debf4d4abb82565d26d71c0cb0d34c
Francisco Javier Cifuentes Cajas <javiercajas90@gmail.com>
2024-09-06 00:33:30 -0600

    [ADD] ValildacionesNombresPDF_WORD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,7 +10,7 @@ class FileUploadController extends Controller
     public function upload(Request $request)
     {
         $request->validate([
-            'file' => 'required|mimes:pdf,doc,docx|max:10240', //max 10 mg
+            'file' => 'required|mimes:pdf,doc,docx|max:10240', //máx. 10 MB
         ]);
 
         $file = $request->file('file');
@@ -19,10 +19,25 @@ class FileUploadController extends Controller
         if (!Storage::exists($destinationPath)) {
             Storage::makeDirectory($destinationPath);
         }
-
-        $fileName = time() . '_' . $file->getClientOriginalName();
+        $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $extension = $file->getClientOriginalExtension();
+        $cleanName = $this->validarNombre($originalName);
+        $fileName = $cleanName . '.' . $extension;
         $file->storeAs($destinationPath, $fileName);
 
         return response()->json(['message' => 'Archivo subido correctamente'], 200);
+    }
+
+    // Función para validar archivo nombre y cambiarlo.
+    private function validarNombre($fileName)
+    {
+        $fileName = preg_replace('/\s+/', ' ', $fileName);
+        $fileName = str_replace(' ', '_', $fileName);
+        $search = ['á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú'];
+        $replace = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        $fileName = str_replace($search, $replace, $fileName);
+        $fileName = str_replace(['Ñ', 'ñ'], ['N', 'n'], $fileName);
+        $fileName = preg_replace('/[°!"#$%&\/(){}=¿?¡¨*\[\];:|\'´\-\+,.\/\\@¬~`^]/', '', $fileName);
+        return $fileName;
     }
 }
