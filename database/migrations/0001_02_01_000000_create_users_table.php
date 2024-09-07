b6977e36a79ea4bb51819ef1bc717bc665528571
Daniel Escobar <danielescobar.987@gmail.com>
2024-09-07 11:19:16 -0600

    [FEAT] Add roles, permissions, user-roles tables and others.

--- database/migrations/0001_02_01_000000_create_users_table.php
+++ database/migrations/0001_02_01_000000_create_users_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('user_name');
             $table->string('email')->unique();
+            $table->string('gender');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('estado');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->boolean('estado')->default(1);
             $table->timestamps();
         });
 
